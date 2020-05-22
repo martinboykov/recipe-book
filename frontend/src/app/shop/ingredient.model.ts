a3a89534d0cdb2162d37da12ffd79fafa43b5cb2
martinboykov <martinboykov@mail.bg>
2020-05-22 09:30:57 +0300

    frontend and backend division

--- frontend/src/app/shop/ingredient.model.ts
+++ frontend/src/app/shop/ingredient.model.ts
@@ -1,5 +1,5 @@
 export class Ingredient {
-  constructor(public name: string, public amount: number, public id?:number) {
+  constructor(public name: string, public amount: number, public id?: number) {
     this.name = name;
     this.amount = amount;
     this.id = id || Math.floor(100000 + Math.random() * 900000);
